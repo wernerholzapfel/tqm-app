a63508e1ca2463261501bf1f6806590e64af031b
wholzapfel <werner.holzapfel@trivento.nl>
2020-04-12 13:16:37 +0200

    take1 pipe toegevoegd aan createquiz

--- src/app/components/create-quiz/create-quiz.component.ts
+++ src/app/components/create-quiz/create-quiz.component.ts
@@ -2,10 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {ModalController} from '@ionic/angular';
 import {QuizService} from '../../services/quiz.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {switchMap} from 'rxjs/operators';
-import {PARTICIPANT_ID, QUIZ_ID} from '../../constants/storage.contants';
-import { Storage } from '@ionic/storage';
-import {FirebaseAuth} from '@angular/fire';
+import {switchMap, take} from 'rxjs/operators';
+import {QUIZ_ID} from '../../constants/storage.contants';
+import {Storage} from '@ionic/storage';
 import {AngularFireAuth} from '@angular/fire/auth';
 
 @Component({
@@ -40,17 +39,17 @@ export class CreateQuizComponent implements OnInit {
                 this.storage.set(QUIZ_ID, quiz.id);
                 return this.quizService.joinQuiz({quiz: {id: quiz.id}, naam: this.createQuizForm.value.naam});
             }))
-            .subscribe(token => {
-                // this.storage.set(PARTICIPANT_ID, participant.id);
-                this.firebase.auth.signInWithCustomToken(token.token).catch(error => {
-                    // Handle Errors here. //todo
-                    console.log(error.message);
-                    // var errorCode = error.code;
-                    // var errorMessage = error.message;
-                    // ...
-                });
-                this.modalController.dismiss(true);
+            .pipe(take(1)).subscribe(token => {
+            // this.storage.set(PARTICIPANT_ID, participant.id);
+            this.firebase.auth.signInWithCustomToken(token.token).catch(error => {
+                // Handle Errors here. //todo
+                console.log(error.message);
+                // var errorCode = error.code;
+                // var errorMessage = error.message;
+                // ...
             });
+            this.modalController.dismiss(true);
+        });
     }
 }
 
